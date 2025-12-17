c99939b608ec8aa283bedb6f78d21e9f4fbf8b38
VISHNU PRIYA T K <tkvishnupriyacse@gmail.com>
2025-12-17 15:26:30 +0530

    Replace input with textarea for question input

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -257,14 +257,15 @@ const Admin = () => {
                       </button>
                     )}
                   </div>
-                  <input
-                    type="text"
+                  <textarea
                     placeholder={`Question ${idx + 1}`}
                     value={q.question}
                     onChange={e => handleQuestionChange(idx, e.target.value)}
-                    className="w-full px-3 py-2 mb-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300 transition"
+                    rows={10}
+                    className="w-full px-3 py-2 mb-2 border rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-pink-300 transition"
                     required
-                  />
+                   />
+
                   <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mb-2">
                     {q.options.map((opt, oIdx) => (
                       <input
